cfe9bd518ce56b0adb846246aec325b912e8d363
Carlos Ceagah <carlosceagah@gmail.com>
2024-08-14 13:06:40 -0300

    feat: create initial routes files

---
+++ src/app/Auth/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+import { S } from './styles'
+export default function Auth() {
+  return(
+    <View style={S.container}>
+      <Text> Auth Screen </Text>
+    </View>
+  )
+}
---
+++ src/app/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { S } from './styles'
+
+export const Home = () => {
+  return(
+    <View style={S.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
